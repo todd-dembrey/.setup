423e57d2d430c4dcb8fc8f750b0834d9bd2108af
Todd Dembrey <todd.dembrey@gmail.com>
2017-01-08 11:29:54 +0000

    Update order of install

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
-apt-get install -y  emacs
-
+#Update pip for both python 2 and 3 system versions
 python2 -m pip install --upgrade pip
 
 python3 -m  pip install --upgrade pip
 
+# Install Emacs
+apt-get install -y  emacs
+
+# Install Emacs dependencies
 python2 -m pip install -r elpy-requirements.txt
