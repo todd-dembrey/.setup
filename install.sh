07751e80ed66a78628900867ddf369b30d01ff97
Todd Dembrey <todd.dembrey@gmail.com>
2017-01-08 16:57:47 +0000

    Automatically install python versions

--- install.sh
+++ install.sh
@@ -2,3 +2,16 @@
 if [ ! -d ~/.pyenv ]; then
     git clone https://github.com/yyuu/pyenv.git ~/.pyenv
 fi
+
+PYVERSIONS=(3.6.0)
+
+for PYTHON in "${PYVERSIONS[@]}"
+do
+    # Install python and skip if existing
+    pyenv install -s $PYTHON
+done
+
+# Concatenate the versions into a single string
+VERSIONSSTRING=$(IFS= ; echo "${PYVERSIONS[*]}")
+# Set the global value so that the new versions are avaliable 
+pyenv global system $VERSIONSSTRING
