169753abb532b05ac5eba217051b0871483bbdc5
Todd Dembrey <todd.dembrey@gmail.com>
2017-02-13 21:27:29 +0000

    Add vim to installed apps

--- sudo-install.sh
+++ sudo-install.sh
@@ -14,3 +14,6 @@ python2 -m pip install -r $SCRIPTPATH/elpy-requirements.txt
 # Install python compilation requirements
 apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
 libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils
+
+# Install other software
+apt-get install vim
