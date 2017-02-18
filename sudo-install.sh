7dbb58b3cff8fa5fb27b91809b1555ac29cd6db1
Todd Dembrey <todd.dembrey@gmail.com>
2017-02-18 09:44:33 +0000

    Add Postgis to install

--- sudo-install.sh
+++ sudo-install.sh
@@ -14,3 +14,13 @@ python2 -m pip install -r $SCRIPTPATH/elpy-requirements.txt
 # Install python compilation requirements
 apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
 libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils
+
+
+# Install postgis
+apt-get install binutils
+apt-get install libproj-dev proj-data proj-bin
+apt-get install libgeos-c1v5 libgeos-dev
+apt-get install gdal-bin libgdal-dev
+apt-get install postgis
+apt-get install postgresql-9.6 postgresql-9.6-postgis-scripts postgresql-server-dev-9.6 python-psycopg2
+
